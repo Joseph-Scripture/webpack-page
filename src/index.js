347444f67224d39c53bed9131bbdb446d6be8d17
Joseph-Scripture <joescript237@gmail.com>
2025-07-25 11:27:18 +0100

    Created basic layout of the page

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+const homeBtn = document.querySelector('.home');
+const aboutBtn = document.querySelector('.about');
+const menuBtn = document.querySelector('.menu');
+const emptyDiv = document.querySelector('#container')
+
+
+import odinImage from "./odin.png";
+   
+// const image = document.createElement("img");
+// image.src = odinImage;
+   
+// document.body.appendChild(image);
